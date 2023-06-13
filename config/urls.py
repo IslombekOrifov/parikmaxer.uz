0c162646c11441c504e50e80b6c6241825a7a639
IslombekOrifov <islombekorifov0199@gmail.com>
2023-06-13 10:49:56 +0500

    architecture of project

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include, re_path
 urlpatterns = [
     re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
 ]
